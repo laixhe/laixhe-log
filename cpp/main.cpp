a1282869ee518ea2c1686b52e7f50c97353ba9af
laixhe <laixhe@laixhe.com>
2021-03-30 23:18:38 +0800

    cpp整理

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,19 +1,5 @@
 #include <iostream>
-#include "Singleton.h"
 
 int main() {
-
-    Singleton& instance_1 = Singleton::getInstance();
-    Singleton& instance_2 = Singleton::getInstance();
-    Singleton::getInstance();
-    Singleton::getInstance();
-
-    instance_1.test();
-    instance_2.test();
-    instance_2.test();
-    instance_2.test();
-    instance_2.test();
-    instance_1.test();
-
     return 0;
 }
